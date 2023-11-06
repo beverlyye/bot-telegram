9d24992b330210907a81c48d3f4cf673f4f10af0
Beverly Adrien Octavianto <beverlyadrienoctavianto@Beverlys-MacBook-Pro.local>
2023-11-06 08:21:39 +0700

    handle message

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ TOKEN: Final = '6581449693:AAEIpCBITfHLgqgDTTH8mr71ucD3gCzaGpU'
 BOT_USERNAME: Final = '@talk2m3bot'
 
 
+#commands 
+
 async def start_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text ('Hi! if you are using this bot, it means that you have a story to share with? please continue love <3')
+    await update.message.reply_text ('Hi! if you are using this bot, it means that you have a story to share with? please continue ;)')
 
 
 async def help_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('I am a listener bot! please type anything to get my response')
 
 
-async def custom_command (update: Update, context: ContextTypes. DEFAULT_TYPE):
+async def custom_command (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text ('This is a custom command!')
 
+
+#responses #buat handle respon dr bot
 
 def take_response(text: str) -> str:
    processed: str = text.lower()
@@ -30,8 +34,9 @@ def take_response(text: str) -> str:
    if 'im having a good day' in processed:
        return 'glad to hear that! what makes you happy?'
    
-   return 'pardon? i  cant understand what you wrote, thankyou'
+   return 'pardon? i  cant understand what you wrote, sorry'
 
+#messages #handle pesan yg akn di sendback ke user(priv/group)
 
 async def take_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_tipe: str = update.message.chat.type
@@ -39,17 +44,4 @@ async def take_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     print(f'User ({update.message.chat.id}) in {message_tipe}: "{text}"')
 
-    if message_tipe == 'group':
-        if BOT_USERNAME in text:
-            new_text: str = text.replace(BOT_USERNAME, ' ').strip()
-            response: str = take_response(new_text)
-        else:
-            return
-    else:
-        response: str = take_response(text)
-
-        print('Bot:', response)
-        await update.message.reply_text(response)
-
-
-
+ 
